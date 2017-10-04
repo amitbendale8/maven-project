bb708e8fc204ffabb8a239d7d5f45c89350829ee
Amit Bendale <amit.bendale@iongroup.com>
2017-10-04 19:59:13 +0530

    java checkins

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,8 +12,11 @@ public class Greeter {
 
   }
 
-  //TODO: Add javadoc comment
-  public String greet(String someone) {
+  /**
+  * @param
+  * @return
+  */
+  public final String greet(String final someone) {
     return String.format("Hello, %s!", someone);
   }
 }
